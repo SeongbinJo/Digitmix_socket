ab07a4c8423503023ebcd2a949f8090993474b6c
avan31017 <avan31017@gmail.com>
2025-05-21 07:45:46 +0900

    [socket] connection - join_room

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require(`express`) // node.js 에서 웹 서버를 만들기 위한 express 프레임워크 가져옴
-const http = require(`http`) // express를 http 서버로 감싸기 위해 HTTP 모듈 가져옴
-const { Server } = require(`socket.io`) // 실시간 통신을 위해 socket.io - Server 가져옴옴
-const cors = require(`cors`) // CORS를 설정하기 위한 모듈. CORS - Cross Origin Resource Sharing, React 프론트와 통신하기위해 필요
+import express from "express"
+import http from "http"
+import { Server } from "socket.io"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
 
@@ -9,9 +12,10 @@ app.use(cors())
 
 const server = http.createServer(app)
 
+// process.env.SIDEPAGE6_DOMAIN
 const io = new Server(server, {
     cors: {
-        origin: import.meta.env.SIDEPAGE6_DOMAIN,
+        origin: `*`,
     },
 })
 
@@ -24,12 +28,12 @@ io.on(`connection`, (socket) => {
         socket.emit(`room_created`, roomId)
     })
 
-    socket.on(`join_room`, (roomId) => {
+    socket.on(`join_room`, ({ roomId, userEmail }) => {
         const room = io.sockets.adapter.rooms
 
         if (room.has(roomId)) {
             socket.join(roomId)
-            console.log(`${socket.id}가 방 ${roomId}에 참가함.`)
+            console.log(`유저(Email: ${userEmail}, socket.id: ${socket.id})가 방 ${roomId}에 참가함.`)
         } else {
             console.log(`${roomId} 방이 존재하지 않음.`)
             socket.emit(`room_not_found`, roomId)
